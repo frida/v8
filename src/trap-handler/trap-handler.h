fa22c3ec68e8a2a7c670c7d296d33888c833b566
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-05-01 21:02:55 +0200

    Disable support for trap handlers
    We don't want them interfering with our exception handling, and we also
    don't want any __thread variables as those will make our dynamic linker
    on i/macOS unnecessarily complex.

--- src/trap-handler/trap-handler.h
+++ src/trap-handler/trap-handler.h
@@ -18,18 +18,7 @@ namespace v8 {
 namespace internal {
 namespace trap_handler {
 
-// TODO(eholk): Support trap handlers on other platforms.
-#if V8_TARGET_ARCH_X64 && V8_OS_LINUX && !V8_OS_ANDROID
-#define V8_TRAP_HANDLER_SUPPORTED true
-#elif V8_TARGET_ARCH_X64 && V8_OS_WIN
-#define V8_TRAP_HANDLER_SUPPORTED true
-#elif V8_TARGET_ARCH_X64 && V8_OS_MACOSX
-#define V8_TRAP_HANDLER_SUPPORTED true
-#elif V8_TARGET_ARCH_X64 && V8_OS_FREEBSD
-#define V8_TRAP_HANDLER_SUPPORTED true
-#else
 #define V8_TRAP_HANDLER_SUPPORTED false
-#endif
 
 struct ProtectedInstructionData {
   // The offset of this instruction from the start of its code object.
@@ -57,14 +46,7 @@ int V8_EXPORT_PRIVATE RegisterHandlerData(
 /// kInvalidIndex.
 void V8_EXPORT_PRIVATE ReleaseHandlerData(int index);
 
-#if V8_OS_WIN
-#define THREAD_LOCAL __declspec(thread)
-#elif V8_OS_ANDROID
-// TODO(eholk): fix this before enabling for trap handlers for Android.
 #define THREAD_LOCAL
-#else
-#define THREAD_LOCAL __thread
-#endif
 
 // Initially false, set to true if when trap handlers are enabled. Never goes
 // back to false then.
