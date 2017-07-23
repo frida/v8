232116a7e788bed35a8d127a7a50bc70d34ff9a3
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-07-23 21:31:21 +0200

    [platform] Add APIs for custom threading primitives

--- src/init/v8.cc
+++ src/init/v8.cc
@@ -11,6 +11,7 @@
 #include "src/base/atomicops.h"
 #include "src/base/once.h"
 #include "src/base/platform/platform.h"
+#include "src/base/platform/threading-backend.h"
 #include "src/codegen/cpu-features.h"
 #include "src/codegen/interface-descriptors.h"
 #include "src/common/code-memory-access.h"
@@ -97,6 +98,7 @@ void V8::InitializePlatform(v8::Platform* platform) {
   CHECK(!platform_);
   CHECK_NOT_NULL(platform);
   platform_ = platform;
+  v8::base::SetThreadingBackend(platform->GetThreadingBackend());
   v8::base::SetPrintStackTrace(platform_->GetStackTracePrinter());
   v8::tracing::TracingCategoryObserver::SetUp();
 #if defined(V8_OS_WIN) && defined(V8_ENABLE_ETW_STACK_WALKING)
